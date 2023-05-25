8af591c8590408928c68ca0c95a6b6baf50796d6
Bryan Ortega <fbryanortega@gmail.com>
2023-05-25 09:09:58 -0500

    Refactored to track and output flesh color, rind color, and average weight.

--- melon_info.py
+++ melon_info.py
@@ -1,18 +1,14 @@
 """Print out all the melons in our inventory."""
 
 
-from melons import melon_names, melon_prices, melon_seedlessness 
+from melons import melon_names, melon_prices, melon_seedlessness, melon_flesh_color, melon_rind_color, melon_average_weight
 
 
-def print_melon(name, price, seedless):
+def print_melon(name, price, seedless, flesh, rind, weight):
     """Print each melon with corresponding attribute information."""
 
-    have_or_have_not = 'have'
-    if seedless:
-        have_or_have_not = 'do not have'
-
-    print(f'{name}s {have_or_have_not} seeds and are ${price}')
+    print(f'{name.upper()}\nprice: {price:.2f}\nseedless: {seedless}\nflesh_color: {flesh}\nrind_color: {rind}\naverage_weight: {weight}')
 
 
 for i in melon_names:
-    print_melon(melon_names[i], melon_prices[i], melon_seedlessness[i])
+    print_melon(melon_names[i], melon_prices[i], melon_seedlessness[i], melon_flesh_color[i], melon_rind_color[i], melon_average_weight[i])
